6b45c6b7f4e3268519ce6722be6036f3c7d229c6
Genadyy <70272737+Genadyy@users.noreply.github.com>
2022-07-20 20:45:53 +0300

    Add MyComponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!!!!</h1>
       <div>Add some more information</div>
+      <MyComponent/>
     </div>
   );
 }
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const MyComponent = () => {
+    return (
+        <div style ={{width: "800px", height: "150px", border: "3px dashed purple"}}>My name is MyComponent</div>
+    )
+}
+
+
+export default MyComponent;
